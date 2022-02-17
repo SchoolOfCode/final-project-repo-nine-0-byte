7dea414e57aeeed8d774d61c2cc9b78e4d4c197c
Samira Sabirin <93654977+samira-s-midgley@users.noreply.github.com>
2022-02-17 16:15:10 +0000

    Create file and html tag

---
+++ components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <img alt="logo" src="" />
+      <h1>Short Circuit</h1>
+      <p>To go or not to go</p>
+    </header>
+  );
+}
