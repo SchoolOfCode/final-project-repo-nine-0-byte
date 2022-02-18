ad001089378473d705359d96d2ef87bd16033e12
Samira Sabirin <93654977+samira-s-midgley@users.noreply.github.com>
2022-02-18 14:00:55 +0000

    creating footer

---
+++ components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <a href="#">About</a>
+      <a href="#">Contact Us</a>
+      <a href="#">Socials</a>
+    </footer>
+  );
+}
+
+// Create 3 links A tag
+// About
+// Contact Us
+// Social (react icons)
