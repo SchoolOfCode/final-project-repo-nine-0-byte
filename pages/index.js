37705877fef2112eb34909ec1dad624f4c685f03
burmanp <kohein@gmail.com>
2022-02-18 12:27:33 +0000

    geo on load added

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,39 @@ import styles from "../styles/Home.module.css";
 import Searchbar from "../components/Searchbar";
 import dynamic from "next/dynamic";
 //import Map from "../components/Map";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [position, setPosition] = useState([]);
+  const [lat, setLat] = useState("");
+  const [lng, setLng] = useState("");
+  const [status, setStatus] = useState("");
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setStatus("Geolocation is not supported by your browser");
+    } else {
+      setStatus("Locating...");
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setStatus(null);
+          setLat(position.coords.latitude);
+          setLng(position.coords.longitude);
+        },
+        () => {
+          setStatus("Unable to retrieve your location");
+        }
+      );
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+    setPosition([Number(lat), Number(lng)]);
+  }, []);
+
+  
+
   const Map = dynamic(() => import("../components/Map"), { ssr: false });
   return (
     <>
@@ -15,8 +46,8 @@ export default function Home() {
         integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
         crossOrigin=""
       />
-      <Searchbar/>
-      <Map />
+      <Searchbar />
+      <Map position={[lat, lng]} />
     </>
   );
 }
