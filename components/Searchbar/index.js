43bc061440edffb50084abb3c6eb5eb258278a14
AleksApilats <shurikapilats@googlemail.com>
2022-02-18 16:30:16 +0000

    finished searchbar and button functionality

--- components/Searchbar/index.js
+++ components/Searchbar/index.js
@@ -1,35 +1,21 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-function Searchbar({ setLocation, searchSubmit }) {
-  const [postcode, setPostcode] = useState("");
-  useEffect(() => {
-    async function getApiLocation() {
-      let request = await fetch(
-        `https://api.postcodes.io/postcodes/${postcode}`
-      );
-      const data = await request.json();
-      setLocation(data.result.latitude, data.result.longitude);
-    }
-    getApiLocation();
-  }, [postcode]);
+function Searchbar({ setPostcode, searchSubmit }) {
+  const [input, setInput] = useState("");
   function inputSearch(e) {
-    setPostcode(e.target.value);
+    setInput(e.target.value);
     console.log("setPostcode", e.target.value);
   }
-
-  let location = {
-    lat: 52.475429,
-    long: -1.884163,
-    postcode: "B9 4AA",
-  };
   return (
-    <div>
-      <input placeholder="Search Bar" onChange={inputSearch} />
-      <button onClick={() => searchSubmit(location.lat, location.long)}>
-        Search
-      </button>
-    </div>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <input onChange={inputSearch} placeholder="Search Bar" />
+      <button onClick={() => setPostcode(input)}>Search</button>
+    </form>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,26 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Searchbar from "../components/Searchbar";
 import dynamic from "next/dynamic";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 //import Map from "../components/Map";
 
 export default function Home() {
-  const [location, setLocation] = useState([51.505, -0.09]);
+  //state and useEffect that is handling our fetching from API poctocdes.io
+  const [postcode, setPostcode] = useState("WC2R 2PP");
+  useEffect(() => {
+    async function getApiLocation() {
+      let request = await fetch(
+        `https://api.postcodes.io/postcodes/${postcode}`
+      );
+      const data = await request.json();
+      console.log(data.result);
+      setLocation([data.result.latitude, data.result.longitude]);
+    }
+    getApiLocation();
+  }, [postcode]);
+
   const Map = dynamic(() => import("../components/Map"), { ssr: false });
+  const [location, setLocation] = useState([51.505, -0.09]);
   function searchSubmit(lat, long) {
     setLocation([lat, long]);
     console.log("Submitted", lat, long);
@@ -21,7 +35,11 @@ export default function Home() {
         integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
         crossOrigin=""
       />
-      <Searchbar setLocation={setLocation} searchSubmit={searchSubmit} />
+      <Searchbar
+        setLocation={setLocation}
+        searchSubmit={searchSubmit}
+        setPostcode={setPostcode}
+      />
       <Map location={location} />
     </>
   );
