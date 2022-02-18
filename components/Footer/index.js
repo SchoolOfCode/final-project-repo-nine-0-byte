6d364d8331a197b389f30a093dc3b2805d1b1ae9
Samira Sabirin <93654977+samira-s-midgley@users.noreply.github.com>
2022-02-18 16:28:14 +0000

    styled footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,11 +1,30 @@
 import React from "react";
+import {
+  AiOutlineInstagram,
+  AiOutlineFacebook,
+  AiOutlineTwitter,
+  AiOutlineLinkedin,
+} from "react-icons/ai";
+import { SiFacebook, SiNextdotjs } from "react-icons/si";
+import { MdAlternateEmail } from "react-icons/md";
+import Style from "./footer.module.css";
 
 export default function Footer() {
   return (
-    <footer>
-      <a href="#">About</a>
-      <a href="#">Contact Us</a>
-      <a href="#">Socials</a>
+    <footer className={Style.Foot}>
+      <div>
+        <a href="#">About</a>
+        <a href="#">Contact Us</a>
+        <a href="#">Socials</a>
+
+        <AiOutlineFacebook />
+        <AiOutlineInstagram />
+        <AiOutlineTwitter />
+        <AiOutlineLinkedin />
+        <SiNextdotjs />
+        <MdAlternateEmail />
+      </div>
+      <p>Copyright ©️ N-O-B</p>
     </footer>
   );
 }
