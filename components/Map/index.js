d4fae5624418588f039a1f7c036dc035298296a4
lkepitter <lkepitter@hotmail.co.uk>
2022-02-18 11:38:44 +0000

    added basic map functionality with Leaflet

--- components/Map/index.js
+++ components/Map/index.js
@@ -1,16 +1,10 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import css from "./map.module.css";
 
 const position = [51.505, -0.09];
 
 export default function Map() {
   return (
-    <MapContainer
-      center={position}
-      zoom={13}
-      scrollWheelZoom={false}
-      className={css.map}
-    >
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
