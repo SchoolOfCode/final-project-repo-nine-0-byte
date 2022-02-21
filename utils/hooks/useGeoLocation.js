2f4df9f4a29e86787bbe4b12cd04c3647fe2491f
burmanp <kohein@gmail.com>
2022-02-21 09:55:42 +0000

    updated initial location state

--- utils/hooks/useGeoLocation.js
+++ utils/hooks/useGeoLocation.js
@@ -1,29 +1,24 @@
-import {useState, useEffect} from 'react';
-export default function useGeoLocation(){
-  const [location, setLocation] = useState([]);
+import { useState, useEffect } from "react";
+export default function useGeoLocation() {
+  const [location, setLocation] = useState([51.505, -0.09]);
   const [error, setError] = useState(null);
-  
-  const onChange = ({coords}) => {
-    setLocation([
-      coords.latitude,
-      coords.longitude,
-    ]);
+
+  const onChange = ({ coords }) => {
+    setLocation([coords.latitude, coords.longitude]);
   };
   const onError = (error) => {
     setError(error.message);
-    setLocation([51.505, -0.09]) // Default if error 
+    setLocation([51.505, -0.09]); // Default if error
   };
   useEffect(() => {
     const geo = navigator.geolocation;
     if (!geo) {
-      setError('Geolocation is not supported');
+      setError("Geolocation is not supported");
       return;
     }
     let watcher = geo.watchPosition(onChange, onError);
     return () => geo.clearWatch(watcher);
   }, []);
 
-  
   return [location, setLocation];
 }
-
