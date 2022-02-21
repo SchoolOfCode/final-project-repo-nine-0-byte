5d81c6a10f1a1df14c7ef3747d56bc217871901e
AleksApilats <shurikapilats@googlemail.com>
2022-02-21 10:30:38 +0000

    dummy data

---
+++ utils/dummy-data/index.js
@@ -0,0 +1,17 @@
+/* Contract:
+IF you call this api with either a postcode or a longitude or lattitude you should expect: 
+An array of up to 10 objects [Obj(10)]
+
+Either object will have 
+name:
+long and lat:
+Connectors: [{type:volatge}]
+FAST: true
+RAPID: false
+SLOW: true
+Available: 
+ETF: int
+Price
+Subscriptions:["Tesla", "SHELL"]
+NearbyPOI:[{"Greggs", latLong}]
+*/
