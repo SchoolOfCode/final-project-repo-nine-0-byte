cde7eb8051e86fede9c1eb2224b8ac6d5ded1194
AleksApilats <shurikapilats@googlemail.com>
2022-02-21 10:48:55 +0000

    created a array of objects

--- utils/dummy-data/index.js
+++ utils/dummy-data/index.js
@@ -5,13 +5,44 @@ An array of up to 10 objects [Obj(10)]
 Either object will have 
 name:
 long and lat:
-Connectors: [{type:volatge}]
-FAST: true
-RAPID: false
-SLOW: true
+Connectors: [{type:volatge}] 
+FAST: true above 7kw
+RAPID: false above 43kw
+SLOW: true below 7kw
 Available: 
 ETF: int
 Price
 Subscriptions:["Tesla", "SHELL"]
 NearbyPOI:[{"Greggs", latLong}]
 */
+
+const dummyData = [
+  {
+    name: "Crewe",
+    lat: 53.09787,
+    long: -2.44161,
+    connectors: [{ type: "10kw" }],
+    fast: true,
+    rapid: false,
+    slow: true,
+    available: true,
+    etf: 15,
+    price: 1,
+    subscription: ["Tesla", "SHELL"],
+    nearbypoi: [{ restaurant: "Greggs" }],
+  },
+  {
+    name: "London",
+    lat: 51.5072,
+    long: 0.1276,
+    connectors: [{ type: "20kw" }],
+    fast: false,
+    rapid: true,
+    slow: true,
+    available: true,
+    etf: 0,
+    price: 5,
+    subscription: ["Tesla", "SHELL"],
+    nearbypoi: [{ restaurant: "Greggs" }],
+  },
+];
