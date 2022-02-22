2d16e27f406a81de060a2573f4279286a8886114
burmanp <kohein@gmail.com>
2022-02-22 16:19:38 +0000

    search overlay position sorted

--- components/Map/index.js
+++ components/Map/index.js
@@ -1,33 +1,37 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import dummyData from "../../utils/dummy-data/index.js";
 import Style from "./map.module.css";
+import Searchbar from "../Searchbar/index.js";
 
 // const position = [51.505, -0.09];
 
 export default function Map({ location }) {
   return (
-    <MapContainer
-      center={location}
-      zoom={16}
-      scrollWheelZoom={false}
-      className={Style.mapContainer}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={location}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily (ustomizable.
-        </Popup>
-      </Marker>
-      {dummyData.map((item, i) => (
-        <Marker position={[item.lat, item.long]} key={i}>
-          <Popup>
-            A pretty Callum popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+    <>
+      <div className={Style.searchOverlay}>
+        <Searchbar />
+      </div>
+      <div className={Style.mapContainer}>
+        <MapContainer center={location} zoom={16} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+          <Marker position={location}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily (ustomizable.
+            </Popup>
+          </Marker>
+          {dummyData.map((item, i) => (
+            <Marker position={[item.lat, item.long]} key={i}>
+              <Popup>
+                A pretty Callum popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+      </div>
+    </>
   );
 }
