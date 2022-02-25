173955f588bfb82700e3d4b30bfe55f2dc336813
Samira Sabirin <93654977+samira-s-midgley@users.noreply.github.com>
2022-02-25 16:15:39 +0000

    create auth0 login page

--- components/Header/index.js
+++ components/Header/index.js
@@ -2,8 +2,24 @@ import React from "react";
 import logo from "../../public/shortcircuitlogo.png";
 import Image from "next/image";
 import Style from "./header.module.css";
+import { useUser } from "@auth0/nextjs-auth0";
 
 export default function Header() {
+  const { user, error, isLoading } = useUser();
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  if (!user) {
+    return <a href="/api/auth/login">Login</a>;
+  }
+  if (user) {
+    return (
+      <div>
+        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+      </div>
+    );
+  }
+
   return (
     <header className={Style.container}>
       <div className={Style.header}>
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
 import LeafletUtils from "../utils/LeafletUtils";
 import Layout from "../components/Layout";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <LeafletUtils />
-      <Component {...pageProps} />
-    </Layout>
+    <UserProvider>
+      <Layout>
+        <LeafletUtils />
+        <Component {...pageProps} />
+      </Layout>
+    </UserProvider>
   );
 }
 
