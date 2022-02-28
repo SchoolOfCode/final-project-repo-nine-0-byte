929e641fbd4aa0dda9ac29dbc7287a31cd1d1b60
burmanp <kohein@gmail.com>
2022-02-28 09:41:26 +0000

    Auth0 works

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,22 +3,23 @@ import logo from "../../public/shortcircuitlogo.png";
 import Image from "next/image";
 import Style from "./header.module.css";
 import { useUser } from "@auth0/nextjs-auth0";
+import Link from "next/link";
 
 export default function Header() {
   const { user, error, isLoading } = useUser();
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
 
-  if (!user) {
-    return <a href="/api/auth/login">Login</a>;
-  }
-  if (user) {
-    return (
-      <div>
-        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
-      </div>
-    );
-  }
+  // if (!user) {
+  //   return <a href="/api/auth/login">Login</a>;
+  // }
+  // if (user) {
+  //   return (
+  //     <div>
+  //       Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+  //     </div>
+  //   );
+  // }
 
   return (
     <header className={Style.container}>
@@ -36,7 +37,20 @@ export default function Header() {
         <h1>Circuit</h1>
         {/* <p>To go or not to go</p> */}
 
-        <button className={Style.login}>Log In</button>
+        <div className={Style.authLink}>
+          {!user && (
+            <div>
+              <p>Login to save your filters</p>
+              <Link href="/api/auth/login">Login</Link>
+            </div>
+          )}
+          {user && (
+            <div>
+              <p>Howdy {user.name}</p>
+              <Link href="/api/auth/logout">Logout</Link>
+            </div>
+          )}
+        </div>
       </div>
     </header>
   );
