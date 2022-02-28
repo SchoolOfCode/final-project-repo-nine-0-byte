44227d907cfe98e28b8a39a8a634b939a127b974
lkepitter <lkepitter@hotmail.co.uk>
2022-02-28 16:13:09 +0000

    working on filter logic. But it's broken :(

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import useGetPOI from "../utils/hooks/useGetPOI";
 import useGetCoordsFromPostcode from "../utils/hooks/useGetCoordsFromPostcode";
 import { useState } from "react";
 import Image from "next/image";
-import Style from "../styles/Home.module.css"
+import Style from "../styles/Home.module.css";
 import Filter from "../components/Filter/Filter";
 
 //import Map from "../components/Map";
@@ -15,12 +15,30 @@ export default function Home() {
   const [setPostcode] = useGetCoordsFromPostcode(setLocation);
   const [isLoading, setIsLoading] = useState(true);
   const [pointsNearby] = useGetPOI(location, setIsLoading);
+  const [markersOn, setMarkersOn] = useState();
+  //Search pointsnearby, find all points with 3-pin
+  //look at all indexes in pointsNearby
+  //in each index, check connectors array
+  // in each connector index, check ConnectorType
+  //IF it's our 3-pin, filter it
+  //Connectors.includes()
+
+  const newArray = pointsNearby.filter((point) => {
+    console.log(
+      point.Connectors.map((connector) => {
+        return connector.ConnectorType === "3-pin Type G (BS1363)";
+      })
+    );
+    return point.Connectors.map(
+      (connector) => connector.ConnectorType === "3-pin Type G (BS1363)"
+    );
+  });
+  console.log("Our array:", newArray);
 
   return (
     <>
+      <Filter />
 
-    {/* <Filter /> */}
-    
       {isLoading && (
         <div className={Style.robot}>
           <Image
