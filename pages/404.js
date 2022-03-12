25ae9faac2dcc47ebcc4da45fe6dde6a04ff2c1a
burmanp <kohein@gmail.com>
2022-03-12 13:41:12 +0000

    404 styles fixed

--- pages/404.js
+++ pages/404.js
@@ -3,26 +3,36 @@ import Image from "next/image";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
+const divStyle = {
+  width: "40vw",
+  margin: "0.5em auto",
+  color: "#f35b04ff",
+};
+const aTagStyle = {
+  fontSize: "1.3rem",
+  color: "#f35b04ff",
+};
+
 function Oops() {
-  // const router = useRouter();
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     router.push("/");
-  //   }, 2200);
-  // }, []);
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3100);
+  }, []);
 
   return (
-    <div style={{"margin-left": "2rem"}}>
-      <h2>Sorry this page cannot be found</h2>
+    <div style={divStyle}>
+      <h1>Sorry this page cannot be found</h1>
       <Image
         src="/FunkyShortCircuitLogo.gif"
         width={500}
-        height={380}
+        height={400}
         alt="logo gif"
       />
       <br />
       <Link href="/">
-        <a>Click here to go back to Homepage</a>
+        <a style={aTagStyle}>Click here to go back to Homepage</a>
       </Link>
     </div>
   );
