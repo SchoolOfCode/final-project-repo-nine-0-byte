068651e08f9e084584c5b5fea1773c140e948de9
burmanp <kohein@gmail.com>
2022-03-13 01:43:11 +0000

    added info cards for the footer links

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -5,9 +5,11 @@ import {
   AiOutlineTwitter,
   AiOutlineLinkedin,
 } from "react-icons/ai";
+import { FiUserCheck } from "react-icons/fi";
 import { SiNextdotjs } from "react-icons/si";
 import { MdAlternateEmail } from "react-icons/md";
 import Style from "./footer.module.css";
+import { Popover } from "antd";
 
 //Ukrainian flag
 setTimeout(
@@ -19,6 +21,47 @@ setTimeout(
     ),
   4000
 );
+const aboutUs = (
+  <div>
+    <p>
+      <FiUserCheck />
+      {" Laura"}
+    </p>
+    <p>
+      <FiUserCheck />
+      {" Pyi"}
+    </p>
+    <p>
+      <FiUserCheck />
+      {" Callum"}
+    </p>
+    <p>
+      <FiUserCheck />
+      {" Aleks"}
+    </p>
+    <p>
+      <FiUserCheck />
+      {" Manni"}
+    </p>
+    <p>
+      <FiUserCheck />
+      {" Sam"}
+    </p>
+  </div>
+);
+const contactUs = (
+  <div>
+    Please feel free to drop us a line at{" "}
+    <a href="mailto:nine.0.byteftw@gmail.com" style={{ color: "white" }}>
+      nine.0.byteftw@gmail.com
+    </a>
+  </div>
+);
+const socials = (
+  <div>
+    Feel free to get in touch with us via any of the socials listed below
+  </div>
+);
 
 export default function Footer() {
   return (
@@ -26,15 +69,36 @@ export default function Footer() {
     <footer className={Style.Foot}>
       <div className={Style.container}>
         <div className={Style.links}>
-          <span>
-            <a href="#">About</a>
-          </span>
-          <span>
-            <a href="#">Contact Us</a>
-          </span>
-          <span>
-            <a href="#">Socials</a>
-          </span>
+          <Popover
+            content={aboutUs}
+            title="Team Nine-0-Byte!"
+            trigger="click"
+            color="#f7b801ff"
+          >
+            <span>
+              <a href="#">About</a>
+            </span>
+          </Popover>
+          <Popover
+            content={contactUs}
+            title="Email us!"
+            trigger="click"
+            color="#7678edff"
+          >
+            <span>
+              <a href="#">Contact Us</a>
+            </span>
+          </Popover>
+          <Popover
+            content={socials}
+            title="Let's chat!"
+            trigger="click"
+            color="#f18701ff"
+          >
+            <span>
+              <a href="#">Socials</a>
+            </span>
+          </Popover>
         </div>
         <div className={Style.socialIcons}>
           <AiOutlineFacebook className={Style.paddingIcon} alt="icon-face" />
@@ -47,7 +111,7 @@ export default function Footer() {
         <div className={Style.copyright}>
           <p>Made with</p>
           <SiNextdotjs
-          size="1.3rem"
+            size="1.3rem"
             className={(Style.paddingIcon, Style.nextIcon)}
             alt="icon-next"
           />
